b0a2e5758ff04273d13159e1b0cf4dbf2ab161c4
Ahmed Hamed <aahmedhhamed@gmail.com>
2016-08-24 12:59:01 +0100

    Update DBconnect.py

--- DBconnect.py
+++ DBconnect.py
@@ -1,9 +1,9 @@
 import MySQLdb
 
 def connection():
-    conn = MySQLdb.connect(host="localhost",
-                           user = "root",
-                           passwd = "root",
-                           db = "therapio_api")
+    conn = MySQLdb.connect(host="",
+                           user = "",
+                           passwd = "",
+                           db = "")
     c = conn.cursor()
     return c, conn
